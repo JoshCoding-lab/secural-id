3edbb19adb2d582338cedcd7174ce6ca23a66c3c
Joshua barani <omogokk@gmail.com>
2025-07-24 20:03:24 +0700

    jdjdj:

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="id">
       <Head>
+        {/* Tambahkan atau pastikan baris ini ada */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        {/* Ini adalah link font dari sebelumnya, biarkan saja */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
