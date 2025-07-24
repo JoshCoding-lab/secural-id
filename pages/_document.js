7c772ba7bd7a582153cfef8b5d912ec341e50970
Joshua barani <omogokk@gmail.com>
2025-07-24 20:05:57 +0700

    jdjjd

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="id">
       <Head>
         {/* Tambahkan atau pastikan baris ini ada */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=12.0" />
         
         {/* Ini adalah link font dari sebelumnya, biarkan saja */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
